c45f9045ac132a10aa021e055fc073750ba77beb
Regie04 <90229394+Regie04@users.noreply.github.com>
2024-12-02 13:05:51 +0800

    Add files via upload

---
+++ WorkoutRepCounter/contracts/Assessment.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract WorkoutRepCounter {
+    int256 public counter; // Counter value
+
+    /// Increment the counter
+    function increment() public {
+        require(counter < type(int256).max, "Counter overflow");
+        counter++;
+    }
+
+    /// Decrement the counter
+    function decrement() public {
+        require(counter > type(int256).min, "Counter underflow");
+        counter--;
+    }
+
+    /// Get the current counter value
+    function getCounter() public view returns (int256) {
+        return counter;
+    }
+}
+
